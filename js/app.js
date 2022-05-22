daa2a0cc9479372405fd5b3ff1fc4eb5d792e52e
hisahito <hisahito706@gmail.com>
2022-05-22 00:38:10 -0500

    adding js logic

---
+++ js/app.js
@@ -0,0 +1,17 @@
+// function que limpia los valores
+function clearScrean() {
+    document.getElementById("result").value = "";
+}
+
+// function que muestra los valores
+
+function display(value) {
+    document.getElementById("result").value += value;
+}
+
+// function que calcula la expresion y la regresa 
+function calculate() {
+    let p = document.getElementById("result").value;
+    let q = eval(p);
+    document.getElementById("result").value = q;
+}
